3b1f6e9945606ee14f4ffabcb3b4668865476484
hi-space <hispace.j@gmail.com>
2026-05-23 12:17:31 +0000

    feat(infra): create 4 customer-managed KMS keys with rotation

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -1,12 +1,15 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NetworkConstruct } from './network/index.js';
+import { KmsConstruct } from './security/kms.js';
 
 export class SearchGatewayStack extends Stack {
   readonly network: NetworkConstruct;
+  readonly kms: KmsConstruct;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.network = new NetworkConstruct(this, 'Network');
+    this.kms = new KmsConstruct(this, 'Kms');
   }
 }
