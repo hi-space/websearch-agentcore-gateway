51f11eb611c738f587bd7664966c511b7c4e3529
hi-space <hispace.j@gmail.com>
2026-05-23 12:10:24 +0000

    feat(infra): VPC with private subnets, NAT, flow logs, interface endpoints

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -1,8 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { NetworkConstruct } from './network/index.js';
 
 export class SearchGatewayStack extends Stack {
+  readonly network: NetworkConstruct;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    this.network = new NetworkConstruct(this, 'Network');
   }
 }
