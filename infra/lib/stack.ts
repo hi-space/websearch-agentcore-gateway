dc4fb38e2dba383d65d16b5ae516e793d611da94
hi-space <hispace.j@gmail.com>
2026-05-23 12:23:42 +0000

    feat(infra): provision Config and Quota DynamoDB tables (CMK, PITR)

--- infra/lib/stack.ts
+++ infra/lib/stack.ts
@@ -2,14 +2,20 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NetworkConstruct } from './network/index.js';
 import { KmsConstruct } from './security/kms.js';
+import { ConfigTableConstruct } from './data/config-table.js';
+import { QuotaTableConstruct } from './data/quota-table.js';
 
 export class SearchGatewayStack extends Stack {
   readonly network: NetworkConstruct;
   readonly kms: KmsConstruct;
+  readonly configTable: ConfigTableConstruct;
+  readonly quotaTable: QuotaTableConstruct;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.network = new NetworkConstruct(this, 'Network');
     this.kms = new KmsConstruct(this, 'Kms');
+    this.configTable = new ConfigTableConstruct(this, 'Config', { kmsKey: this.kms.ddbKey });
+    this.quotaTable = new QuotaTableConstruct(this, 'Quota', { kmsKey: this.kms.ddbKey });
   }
 }
